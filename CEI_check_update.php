2e36537ed4716a61d73b9c72d4f7a98d614474a0
Guillaume Puyal <contact@guillaume-puyal.com>
2014-06-07 22:29:14 +0200

    Add 0.4 release to the update system web backend

--- CEI_check_update.php
+++ CEI_check_update.php
@@ -3,6 +3,9 @@
 $version = $_GET["v"];
 
 switch($version){
+	case "0.3":
+		echo "0.4;https://github.com/Guillaume201/Importer-Hub-for-CRYENGINE/releases/tag/0.4";
+		break;
 	case "0.2":
 		echo "0.3;https://github.com/Guillaume201/Importer-Hub-for-CRYENGINE/releases/tag/0.3";
 		break;
